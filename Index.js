2b491a305542b45f2f12be55f2c6c14eba46fd13
belie <92064787+BeleteK@users.noreply.github.com>
2021-11-10 19:46:37 -0500

    Add files via upload
    new changes

--- Index.js
+++ Index.js
@@ -1,1 +1,35 @@
-console.log('Hello consol message')
+let MyName='Bele'
+let myfathername='Bedasa'
+console.log('Hello',MyName+' '+myfathername)
+
+let Comentator='lema'
+let friendname='tariku'
+// console.log('Hello',Comentator+' '+friendname)
+
+let mydepartiment='chemistry'
+let myschool='goro'
+// console.log('Hello',mydepartiment+' '+myschool)
+
+let belete = {
+    name:'belete',
+    age: 20,
+    height: 100,
+}
+
+console.log("belte's age is", belete.age)
+console.log("belete's height", belete.height)
+function sayhello(a){
+    console.log(a)
+}
+function add(a,b){
+    console.log(a+b)
+}
+
+// sayhello('welcome')
+// add(55,77)
+
+function subtract(x,y){
+    console.log(x-y)
+}
+subtract(45,98)
+subtract(195,120)
